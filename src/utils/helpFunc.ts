991f1d037430715b28c206d9fe4c620494cbe630
Nam Le <nam.leduc02@gmail.com>
2020-09-08 10:54:51 +0200

    replace moment with date-fns

--- src/utils/helpFunc.ts
+++ src/utils/helpFunc.ts
@@ -1,10 +1,14 @@
 import { hours, weekdays, mappingWeekToArrayIndex } from '../config/constant';
 import { DayObj, DayOfWeek } from '../types';
 // import moment from 'moment';
-import moment from 'moment-timezone';
+// import moment from 'moment-timezone';
+import toDate from 'date-fns/toDate';
+import { utcToZonedTime, format } from 'date-fns-tz';
 
 export const processData = (valueArr: number[], timestampArr: number[]) => {
   const keepTrackWeek: Array<{ [key: string]: number }> = [];
+  const timeZone = 'Europe/Berlin';
+
   const templateTable = weekdays.map(weekday => {
     const obj: DayObj = { date: weekday };
     hours.map(hour => {
@@ -16,9 +20,12 @@ export const processData = (valueArr: number[], timestampArr: number[]) => {
   });
 
   timestampArr.map((timestamp, idx) => {
-    const date = moment(timestamp).tz('Europe/Athens');
-    const dayOfWeek = date.locale('en').format('ddd') as DayOfWeek;
-    const hour = date.format('HH');
+    // const date = moment(timestamp).tz('Europe/Berlin');
+    // const dayOfWeek = date.locale('en').format('ddd') as DayOfWeek;
+    // const hour = date.format('HH');
+    const zonedDate = utcToZonedTime(toDate(timestamp), timeZone);
+    const dayOfWeek = format(zonedDate, 'eee', { timeZone }) as DayOfWeek;
+    const hour = format(zonedDate, 'HH', { timeZone });
 
     if (dayOfWeek !== 'Sun' && hours.includes(hour)) {
       templateTable[mappingWeekToArrayIndex[dayOfWeek]][hour] += valueArr[idx];
